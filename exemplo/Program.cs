970d22dafb7abc2c83eab7b5f0edc95690b09604
Danilo Keithi Makiya Nakasone <100208589+DaniloKeithi@users.noreply.github.com>
2023-03-07 11:08:59 -0300

    Add files via upload

---
+++ exemplo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace exemplo;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        //Console.ReadLine();
+        //INSTANCIA DE UM OBJETO DA CLASSE AVIAO
+        Aviao tecoteco = new Aviao();
+
+        //USANDO ATRIBUTOS E/OU METODOS DA CLASSE PELO OBJETO CRIADO
+        tecoteco.acelerar();
+
+        //USANDO METODO SEM RETORNO E COM PARAMETRO
+
+        tecoteco.acelerarParametro("Danilo Keithi");
+
+
+    //METODO COM RETORNO E SEM PARAMETROS
+
+    tecoteco.potato();
+
+    Console.WriteLine(tecoteco.potato());
+
+
+    //METODO COM RETORNO E COM PARAMETRO
+
+    Console.WriteLine(tecoteco.somar(20,50));
+
+
+
+    }
+}
