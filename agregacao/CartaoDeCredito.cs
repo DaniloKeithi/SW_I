dd73f7643a9bab7ba1ef6383a1e9d9b09cff1028
Danilo Keithi Makiya Nakasone <100208589+DaniloKeithi@users.noreply.github.com>
2023-04-11 09:48:58 -0300

    Add files via upload

---
+++ agregacao/CartaoDeCredito.cs
@@ -0,0 +1,9 @@
+namespace agregacao;
+
+class CartaoDeCredito
+{
+    public string? Numero {get;set;}
+    public string? DataValidade {get;set;}
+    public Cliente? Cliente {get;set;}
+
+}
---
+++ agregacao/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace agregacao;
+class Program
+{
+  static void Main(string[]args)
+{
+    Console.WriteLine("Trabalhando com agregação");
+
+    //instanciando os objetos de cada classe
+    CartaoDeCredito cdc = new CartaoDeCredito();
+    Cliente cli = new Cliente();
+
+    //Adicionando um nome para o cliente
+    cli.Nome = "Danilo Keithi";
+
+    //Adicionando número e validade do Cartão de Crédito
+    cdc.Numero = "400289222";
+    cdc.DataValidade = "4/2027";
+
+    //Associando o cliente ao atributo de agregação em Cartão de Crédito
+    cdc.Cliente = cli;
+
+    //Visualizando as informações
+    Console.WriteLine("O nome do cliente é: " + cli.Nome);
+    Console.WriteLine("O numero do cartão é: " + cdc.Numero);
+    Console.WriteLine("A data de validade é: " + cdc.DataValidade);
+    Console.WriteLine("O nome do cliente agregado é: " + cdc.Cliente.Nome);
+}
+}
