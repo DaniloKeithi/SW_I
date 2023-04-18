fca91a1bb7ca72d6f6321f0a06caae52e9e866fb
Danilo Keithi Makiya Nakasone <100208589+DaniloKeithi@users.noreply.github.com>
2023-04-18 11:09:24 -0300

    Add files via upload

---
+++ Juros/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Juros;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Banco b = new Banco();
+        
+        Console.WriteLine("Digite o valor de emprestimo");
+        B.emprestimo = double.Parse(Console.ReadLine());
+        
+        
+    }
+}
