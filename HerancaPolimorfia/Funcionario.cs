09473b206c42a3bd9a877707c1190e28971aa9a6
Danilo Keithi Makiya Nakasone <100208589+DaniloKeithi@users.noreply.github.com>
2023-05-02 10:53:59 -0300

    Add files via upload

---
+++ HerancaPolimorfia/Funcionario.cs
@@ -0,0 +1,12 @@
+namespace HerancaPolimorfia;
+class Funcionario
+{  
+   
+public string? Nome {get;set;}
+public int? Idade {get;set;}
+
+    
+public virtual double Participacao(){
+    return 12000* 0.03;
+    }
+}
---
+++ HerancaPolimorfia/FuncionarioN3.cs
@@ -0,0 +1,9 @@
+namespace HerancaPolimorfia;
+class FuncionarioN3: Funcionario
+{  
+    public string? Cargo {get; set;}
+    
+public override double Participacao(){
+    return base.Participacao()* 0.03;
+ }
+}
